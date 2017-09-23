8c70edfa7a1851488d019477e23b056990028eb8
Kendal Gates <leoneusg@yahoo.com>
2017-09-23 20:56:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function add(a,b) {
   return 1 + 80
 }
-function subtract() {
+function subtract(a,b) {
   return 60 - 40
 }
-function multiply() {
+function multiply(a,b) {
   return 2 * 3.4
 }
-function divide() {
+function divide(a,b) {
   return 5.0 / 2.5
 }
